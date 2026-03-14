b44563a705b85e140a6277ddfcc01b715b8b3593
John Henly M. Zapata <henlykun@gmail.com>
2026-03-14 19:45:09 +0800

    added function to date in activity_schedule.xml. Now its automatically update base on days and month. Additionally, the selected date number will be highlighted by bg_dayselected.xml. few tweaks in days UI, made it longer and made the text area wider.

--- app/src/main/java/com/example/exp1/ScheduleActivity.java
+++ app/src/main/java/com/example/exp1/ScheduleActivity.java
@@ -2,11 +2,20 @@ package com.example.exp1;
 
 import android.os.Bundle;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import org.w3c.dom.Text;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 public class ScheduleActivity extends AppCompatActivity {
 
@@ -22,6 +31,7 @@ public class ScheduleActivity extends AppCompatActivity {
             return insets;
         });
 
+
         ImageButton backBtn = findViewById(R.id.imageButton);
         if (backBtn != null) {
             backBtn.setOnClickListener(v -> finish());
@@ -29,5 +39,45 @@ public class ScheduleActivity extends AppCompatActivity {
 
         // Setup bottom navigation
         NavigationHelper.INSTANCE.setupBottomNavigation(this);
+
+        //calendar
+        TextView monthText = findViewById(R.id.month);
+
+        //Upd month and add year beside the month so it will be like March 2026
+        Calendar calendar = Calendar.getInstance();
+        SimpleDateFormat monthFormat = new SimpleDateFormat("MMMM yyyy", Locale.getDefault());
+
+        String currentMonth = monthFormat.format(calendar.getTime());
+        monthText.setText(currentMonth);
+
+       // array of days
+        TextView[] days = {
+                findViewById(R.id.day1),
+                findViewById(R.id.day2),
+                findViewById(R.id.day3),
+                findViewById(R.id.day4),
+                findViewById(R.id.day5),
+                findViewById(R.id.day6),
+                findViewById(R.id.day7)
+        };
+
+
+        // get today's date
+        int today = calendar.get(Calendar.DAY_OF_MONTH);
+
+        // fill the 7 days
+        for(int i = 0; i < 7; i++){
+
+            int dayNumber = calendar.get(Calendar.DAY_OF_MONTH);
+            days[i].setText(String.valueOf(dayNumber));
+
+            // highlight today
+            if(dayNumber == today){
+                days[i].setBackgroundResource(R.drawable.bg_dayselected);
+            }
+
+            calendar.add(Calendar.DAY_OF_MONTH, 1);
+        }
+
     }
 }
